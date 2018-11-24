41e14424df7d5128305f951cb4079e6c0a745307
HoChiak <30897048+HoChiak@users.noreply.github.com>
2018-11-24 14:40:32 +0100

    Delete execute.py

--- execute.py
+++
@@ -1,8 +0,0 @@
-from module_pdf_link import PDFweblink
-
-link = r'http://www.ima.uni-stuttgart.de/studium/stud_arbeiten/bereich_zuv/index.html'
-path = r'C:\Users\%USERNAME%\Desktop'
-
-StudArbeiten = PDFweblink(link, path)
-StudArbeiten.fetch_pdflinks()
-StudArbeiten.download_pdfs()
